0684dd9dba89a3e9a2223b9d2ba69a09836d5787
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-16 15:27:38 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,20 +1,23 @@
 class CashRegister
   
-  def initialize
+  attr_accessor :total, :add_item, :apply_discount, :items, :void_last_transaction
+  
+  def initialize(discount = 0)
     @total = 0
+    @discount 
   end 
   
-  def total 
-    @total
-  end 
+
   
   def add_item
   end 
   
   def apply_discount
+    @apply_discount = apply_discount
   end 
   
-  def items
+  def items(title, price)
+    
   end 
   
   def void_last_transaction
