56906726046cf1e4b0de9d0df432f6cd01037f01
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-16 13:09:05 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
-
+class CashRegister
+  
+  def initialize
+    @total = 0
+  end 
+  
+  def total 
+    @total
+  end 
+  
+  def add_item
+  end 
+  
+  def apply_discount
+  end 
+  
+  def items
+  end 
+  
+  def void_last_transaction
+  end 
+  
+  
+  
+end 
